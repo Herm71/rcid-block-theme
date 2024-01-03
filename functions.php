4f00fd75027cea60b183f2226d7009108179c960
Jason Chafin <jchafin@ucsc.edu>
2024-01-03 08:19:24 -0800

    Final changes

--- functions.php
+++ functions.php
@@ -58,12 +58,9 @@ function rcid_scripts() {
 	wp_enqueue_style( 'rcid-google-lato-font', 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap', false );
 	wp_register_script( 'rcid-front', get_template_directory_uri() . '/build/theme.js', array(), wp_get_theme()->get( 'Version' ), true );
 	wp_enqueue_script( 'rcid-front' );
-	wp_enqueue_style(
-		'dashicons'
-	);
+	wp_enqueue_style( 'dashicons' );
 }
 add_action( 'wp_enqueue_scripts', 'rcid_scripts' );
-
 
 
 /**
@@ -75,43 +72,3 @@ function rcid_googleapi_scripts() {
 }
 add_action( 'wp_head', 'rcid_googleapi_scripts' );
 
-/**
- * Breadcrumbs constructor callback helper
- *
- * @return string
- */
-function rcid_breadcrumbs_constructor() {
-	$labels = array(
-		'title' => '',
-	);
-	$args   = array(
-		'labels'         => $labels,
-		'show_on_front'  => true,
-		'show_trail_end' => false,
-	);
-	return Hybrid\Breadcrumbs\Trail::render( $args );
-}
-
-/**
- * Add Breadcrumbs above Post Title.
- *
- * @param  string $block_content Block content to be rendered.
- * @param  array  $block         Block attributes.
- * @return string
- */
-function rcid_add_breadcrumbs( $block_content = '', $block = array() ) {
-	if ( rcid_breadcrumbs_constructor() ) {
-		$breadcrumbs = rcid_breadcrumbs_constructor();
-	}
-	if ( is_singular() && isset( $breadcrumbs ) ) {
-		if ( isset( $block['blockName'] ) && 'core/post-title' === $block['blockName'] ) {
-			if ( isset( $block['attrs']['level'] ) && isset( $block['attrs']['className'] ) && $block['attrs']['className'] === 'primary-post-title' ) {
-				$html = str_replace( $block_content, $breadcrumbs . $block_content, $block_content );
-				return $html;
-			}
-		}
-	}
-	return $block_content;
-	var_dump($breadcrumbs);
-}
-add_filter( 'render_block', 'rcid_add_breadcrumbs', 10, 2 );
